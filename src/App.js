4a0a9980a05ca2c8c32e87486bed243523c03195
Minh Duc PHUNG <md.phung@groupeonepoint.com>
2023-06-06 14:22:18 +0200

    trying to fix the importance button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
 import noteService from "./services/notes";
-import Notification from "./components/Notification";
 import Error from "./components/Error";
 
 const Footer = () => {
